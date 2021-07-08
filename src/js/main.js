a32824cd2cab2e951de41f6bd96f1932f3c1fc05
judigot <judigot@github.com>
2021-07-08 19:14:29 +0800

    Commit

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/scss/bootstrap-utilities.scss';
+// import 'bootstrap/scss/_modal.scss';
+// import 'jquery/dist/jquery.min.js';
+// import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+// import $ from "jquery";
+// var $ = require('jquery');
+
+// const { JSDOM } = require("jsdom");
+// const { window } = new JSDOM("");
+// const $ = require("jquery")(window);
+
+
+// $(document).ready(function () {
+//     $('#exampleModal').modal('show');
+// });
