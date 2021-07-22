f5af3946812830d532783e2817ecf3ed939ed580
judigot <judigot@github.com>
2021-07-22 14:01:52 +0800

    Commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-import express from "express";
-const app = express();
-const port = process.env.PORT || 3000; // Production environment port may not be 5000, hence the "process.env.PORT"
+
+// Set up Express
+import express, { Application, Request, Response, NextFunction } from "express";
+
+const app: Application = express();
+// Variables
+// *production environment port may not be 5000, hence the "process.env.PORT"
+const PORT = process.env.PORT || 3000;
+
 const path = require("path");
+const viewsFolder: string = `views`;
 
 // Set view engine
-app.set("views", path.resolve(__dirname, `views`));
+app.set("views", path.resolve(__dirname, viewsFolder));
+
 app.set("view engine", "ejs");
 
 var Database = require("./app/Classes/Database.ts");
@@ -15,14 +23,12 @@ var DB = new Database();
 
 console.log(DB.helloWorld());
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.render("index.ejs", { name: "Hey" });
 });
 
 // Initialize port
-app.listen(port, () => {
-  console.log(`Port has started at http://localhost:${port}`);
-});
+app.listen(PORT);
 
 // Serve static pages without template engines
 // app.use(express.static(path.resolve(__dirname, "public")));
