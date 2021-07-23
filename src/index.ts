790dd407d5ebb934a4f28334f30d1552ca3269a8
judigot <judigot@github.com>
2021-07-23 18:26:56 +0800

    Commit

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ require("dotenv").config();
 
 // *production environment port may not be 5000, hence the "process.env.PORT"
 const PORT = process.env.PORT || 3000;
-console.log(PORT);
 
 // Load environment
 import express, { Application, Request, Response, NextFunction } from "express";
