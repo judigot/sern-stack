3157dfcf75deb9ddd354fa1f418ca6ec8aa6d4e8
judigot <judigot@github.com>
2021-07-25 17:44:58 +0800

    Commit

--- src/app/Classes/Database.ts
+++ src/app/Classes/Database.ts
@@ -1,11 +1,65 @@
 class Database {
-  private message: string = "Hello, World!";
+  private message: any = "Hello, World!";
 
   constructor() {}
 
   public helloWorld() {
     return this.message;
   }
+
+  public create(tableName: any, columnNames: any, data: any)
+    {
+        
+    }
+
+    public read(sql: any)
+    {
+       
+    }
+
+    public update(tableName: any, targetColumn: any, newValue: any, referenceColumn: any, referenceValue: any)
+    {
+    }
+
+    public delete(tableName: any, referenceColumn: any, referenceValue: any)
+    {
+        
+    }
+
+    public execute(sql: any)
+    {
+        
+    }
+
+    public duplicate(tableName: any, referenceColumn: any, referenceValue: any, incrementColumn: any, incrementString: any)
+    {
+        
+    }
+
+    public getCredentials()
+    {
+        
+    }
+
+    public connect()
+    {
+        
+    }
+
+    public disconnect(connection: any)
+    {
+        connection = null;
+    }
+
+    public dump()
+    {
+        
+    }
+
+    public unionBuilder(iterator: any, tableDetails: any)
+    {
+        
+    }
 }
 
 export = Database;
