1d6b38debfd6e39cc433015f577a01db57eb0a53
judigot <judigot@github.com>
2021-08-03 19:34:35 +0800

    Commit

--- src/index.js
+++ src/index.js
@@ -26,6 +26,23 @@ const db = new Database();
  *********/
 //================================================================================//
 db.connect();
+
+// get the client
+const mysql = require("mysql2");
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "bigbang",
+});
+
+// simple query
+connection.query("SELECT * FROM `users`", function (err, results, fields) {
+  console.log(results[0]); // results contains rows returned by server
+  // console.log(fields); // fields contains extra meta data about results, if available
+});
+
 //================================================================================//
 
 // Set up route
