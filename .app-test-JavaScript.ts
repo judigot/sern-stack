c0eaf483a0dbf35df8299a3eb55ceacfe0693bb6
judigot <judigot@github.com>
2022-03-05 10:52:16 +0800

    Commit

--- .app-test-JavaScript.ts
+++ .app-test-JavaScript.ts
@@ -2,6 +2,17 @@ import DB from "./src/app/Classes/Database";
 
 import Auth from "./src/app/Http/Controllers/AuthenticationController";
 
+const Models = require("./src-js/models");
+
+Models.User.create({
+  firstName: "Judy",
+  lastName: "Igot",
+  email: "judigot@gmail.com",
+  password: "$2b$10$hi41dPYJv0a6NcnvrUFVqevSI5Ehxzp29yNvAkD.GXfuz98Mlt0wq",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+});
+
 // Auth.hashPassword("123").then((hash) => {
 //   DB.create("users", {
 //     firstName: "Judy Gwapo",
