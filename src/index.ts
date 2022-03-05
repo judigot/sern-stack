1ea0f2158cdc754b733e728c19c67ef6763bbc05
judigot <judigot@github.com>
2022-03-05 13:05:59 +0800

    Commit

--- src/index.ts
+++ src/index.ts
@@ -24,37 +24,37 @@ app.use(express.json());
 import DB from "./app/Classes/Database";
 
 //================================================================================//
-DB.update("users", { lastName: "00000" }).then(() => {
-  DB.update("users", { lastName: "11111" }, { id: 1 }).then(() => {
-    DB.update("users", { lastName: "22222" }, undefined, { id: [2, 3] }).then(
-      () => {
-        DB.update("users", { lastName: "33333" }, { id: [4, 5] }).then(() => {
-          DB.update(
-            "users",
-            { lastName: "44444" },
-            { id: 6 },
-            { id: [7, 8] }
-          ).then(() => {
-            DB.update(
-              "users",
-              { lastName: "55555", email: "Success" },
-              { id: 9 },
-              { id: [10] }
-            ).then(() => {
-              // Multiple where statements
-              DB.update(
-                "users",
-                { email: "Success" },
-                { id: 1, firstName: "Judyboy" },
-                { id: [3, 4] }
-              );
-            });
-          });
-        });
-      }
-    );
-  });
-});
+// DB.update("users", { lastName: "00000" }).then(() => {
+//   DB.update("users", { lastName: "11111" }, { id: 1 }).then(() => {
+//     DB.update("users", { lastName: "22222" }, undefined, { id: [2, 3] }).then(
+//       () => {
+//         DB.update("users", { lastName: "33333" }, { id: [4, 5] }).then(() => {
+//           DB.update(
+//             "users",
+//             { lastName: "44444" },
+//             { id: 6 },
+//             { id: [7, 8] }
+//           ).then(() => {
+//             DB.update(
+//               "users",
+//               { lastName: "55555", email: "Success" },
+//               { id: 9 },
+//               { id: [10] }
+//             ).then(() => {
+//               // Multiple where statements
+//               DB.update(
+//                 "users",
+//                 { email: "Success" },
+//                 { id: 1, firstName: "Judyboy" },
+//                 { id: [3, 4] }
+//               );
+//             });
+//           });
+//         });
+//       }
+//     );
+//   });
+// });
 
 //================================================================================//
 
