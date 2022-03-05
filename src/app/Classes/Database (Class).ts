fda8a0211922ef792d886e33151a7c0ff49d9084
judigot <judigot@github.com>
2022-03-05 12:52:42 +0800

    Commit

--- src/app/Classes/Database (Class).ts
+++ src/app/Classes/Database (Class).ts
@@ -15,6 +15,16 @@ class Database {
   private static username: any = process.env.DB_USERNAME;
   private static password: any = process.env.DB_PASSWORD;
 
+  private static connection: any = {
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  };
+
   private static pool: any = DB.createConnection({
     host: Database.host,
     user: Database.username,
