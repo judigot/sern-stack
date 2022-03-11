55b2f82f54d68c8b7a48371c5e36643cc5784ddf
judigot <judigot@github.com>
2022-03-11 11:44:47 +0800

    Commit

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,9 @@ app.set("view engine", "ejs");
 
 app.use(express.json());
 
+// Serve static files (CSS, JS, images, fonts, etc.)
+app.use(express.static(path.resolve(__dirname, "public")));
+
 //================================================================================//
 import routes from "./routes/web";
 
@@ -28,44 +31,8 @@ app.use("/", routes);
 // app.use("/user", userRoutes);
 
 //================================================================================//
-// DB.update("users", { lastName: "00000" }).then(() => {
-//   DB.update("users", { lastName: "11111" }, { id: 1 }).then(() => {
-//     DB.update("users", { lastName: "22222" }, undefined, { id: [2, 3] }).then(
-//       () => {
-//         DB.update("users", { lastName: "33333" }, { id: [4, 5] }).then(() => {
-//           DB.update(
-//             "users",
-//             { lastName: "44444" },
-//             { id: 6 },
-//             { id: [7, 8] }
-//           ).then(() => {
-//             DB.update(
-//               "users",
-//               { lastName: "55555", email: "Success" },
-//               { id: 9 },
-//               { id: [10] }
-//             ).then(() => {
-//               // Multiple where statements
-//               DB.update(
-//                 "users",
-//                 { email: "Success" },
-//                 { id: 1, firstName: "Judyboy" },
-//                 { id: [3, 4] }
-//               );
-//             });
-//           });
-//         });
-//       }
-//     );
-//   });
-// });
-
-//================================================================================//
 
 //================================================================================//
 
 // Initialize port
 app.listen(PORT);
-
-// Serve static files (CSS, JS, images, fonts, etc.)
-app.use(express.static(path.resolve(__dirname, "public")));
