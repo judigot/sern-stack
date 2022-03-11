a597edf7d31f67a865d2ee4b700dd2d08aac09d1
judigot <judigot@github.com>
2022-03-11 18:40:50 +0800

    Commit

--- Webpack.ts
+++ Webpack.ts
@@ -3,13 +3,7 @@ import { buildType } from "./webpack.config";
 
 const env = process.argv.slice(2)[0];
 
-const compiler = webpack(buildType(env), (error?: any, stats?: any) => {
-  if (error || stats?.hasErrors()) {
-    console.log(error || stats);
-  }
-  console.log("Done processing.");
-});
-compiler.run((error?, stats?) => {
+webpack(buildType(env), (error?: any, stats?: any) => {
   if (error || stats?.hasErrors()) {
     console.log(error || stats);
   }
