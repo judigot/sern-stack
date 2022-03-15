a5a59d2cc1d76ccc721fe59d3779bfec343e5bf0
judigot <judigot@github.com>
2022-03-15 21:54:33 +0800

    Commit

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,14 +1,68 @@
+import React from "react";
+import { FormEventHandler, useRef, useState } from "react";
+
 interface Props {}
 
+interface Form {
+  username: string | undefined;
+  password: string | undefined;
+}
+
 export const LoginForm = (props: Props) => {
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const [username, setUsername] = React.useState<Form["username"]>();
+  const [password, setPassword] = React.useState<Form["password"]>();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(username);
+    alert(password);
+
+    // const target = e.target as typeof e.target & {
+    //   username: { value: string };
+    //   password: { value: string };
+    // };
+    // const username: string = target.username.value; // typechecks!
+    // const password = target.password.value; // typechecks!
+    // setUsername(username);
+    // setPassword(password);
+  };
+
+  const handleChange = (e: React.KeyboardEvent) => {
+    const usernameVal: Form["username"] = usernameRef.current?.value;
+    const passwordVal: Form["password"] = passwordRef.current?.value;
+    setUsername(usernameVal);
+    setPassword(passwordVal);
+  };
+
   return (
     <div className="login-form">
-      <form>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
         <label>Username</label>
-        <input type="text" name="email" />
+        <input
+          ref={usernameRef}
+          onKeyUp={(e) => {
+            handleChange(e);
+          }}
+          type="text"
+          name="username"
+        />
 
         <label>Password</label>
-        <input type="password" name="password" />
+        <input
+          ref={passwordRef}
+          onKeyUp={(e) => {
+            handleChange(e);
+          }}
+          type="password"
+          name="password"
+        />
         <button type="submit">Login</button>
       </form>
     </div>
