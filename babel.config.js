7554e99bb22b4b00c76f37e8b93195c4f27f0505
judigot <judigot@github.com>
2022-03-24 22:15:58 +0800

    Commit

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,19 @@
-export default {
+// Extract paths from tsconfig.json and convert to aliases
+const tsconfig = require("./tsconfig.json");
+const paths = tsconfig.compilerOptions.paths;
+let aliases = {};
+for (let i = 0; i < Object.keys(paths).length; i++) {
+  const key = Object.keys(paths)[i];
+
+  // Remove / and * from the string
+  const alias = key.replace(/\/\*/g, "");
+
+  // Babel.config.js
+  const pathToFolder = paths[key][0].replace(/\*/g, "").slice(0, -1);
+  aliases[alias] = `./src/${pathToFolder}/`;
+}
+
+module.exports = {
   presets: [
     "@babel/preset-env",
     "@babel/preset-react",
@@ -9,4 +24,14 @@ export default {
       plugins: ["@babel/plugin-transform-runtime"],
     },
   },
+  plugins: [
+    ["@babel/plugin-transform-runtime"], // Fixes "ReferenceError: regeneratorRuntime is not defined"
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        alias: aliases,
+      },
+    ],
+  ],
 };
