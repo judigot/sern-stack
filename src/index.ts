bc3c1f042ff601183937d344526bddb2f2847bb2
judigot <judigot@github.com>
2022-03-28 14:36:18 +0800

    Commit

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,32 @@ import express, { Application } from "express";
 const app: Application = express();
 const viewsFolder: string = `views`;
 
+//==========CORS==========//
+// Disable CORS errors; Enable requests from front-end
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*"); // Allow all websites to access the server
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Allow only specific sites to access the server
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH");
+    return res.status(200).json({});
+  }
+  //   if (req.method === "POST") {
+  //     return res.status(404).json({}); // Send a 404 response to the browser
+  //   }
+  next();
+});
+//==========CORS==========//
+
+// Runs every time a route is accessed
+// app.use(function (req, res, next) {
+//   console.log("Hello, World!");
+//   next();
+// });
+
 // Set view engine
 app.set("views", path.resolve(__dirname, viewsFolder));
 app.set("view engine", "ejs");
