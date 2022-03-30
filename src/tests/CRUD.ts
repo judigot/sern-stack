38879fe6814cb8b63322ee51ff7307d90854729c
judigot <judigot@github.com>
2022-03-30 23:18:03 +0800

    Commit

---
+++ src/tests/CRUD.ts
@@ -0,0 +1,22 @@
+import DB from "../app/Classes/Database";
+
+import Auth from "../app/Http/Controllers/AuthenticationController";
+
+// Re-seed data before running CRUD test
+
+DB.read("SELECT * from `users`;").then((result: any) => {
+  console.log(result);
+});
+
+Auth.hashPassword("123").then((hash) => {
+  DB.create("users", {
+    firstName: "Jude",
+    lastName: "Igot",
+    email: "judigot@gmail.com",
+    password: hash,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }).then(async (result) => {
+    console.log(result);
+  });
+});
