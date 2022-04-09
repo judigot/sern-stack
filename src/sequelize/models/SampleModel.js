d31ad97f03f22afffd0366b3457a882218d44728
judigot <judigot@github.com>
2022-04-09 18:05:02 +0800

    Commit

--- src/sequelize/models/SampleModel.js
+++ src/sequelize/models/SampleModel.js
@@ -43,7 +43,26 @@ const User = sequelize.define("User", {
 });
 
 (async () => {
-  await sequelize.sync({ force: true });
+  await sequelize
+    .query("SET FOREIGN_KEY_CHECKS = 0")
+    .then(function () {
+      sequelize
+        .sync({
+          force: true,
+        })
+        .then(function () {
+          sequelize.query("SET FOREIGN_KEY_CHECKS = 1").then(function () {
+            console.log("Database synchronised.");
+          });
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    })
+    .catch(function (ee) {
+      console.log(err);
+    });
+  // await sequelize.sync({ force: true });
   // Code here
 })();
 export default User;
