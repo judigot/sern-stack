bb13f67253a640233140e37eea6b1510a0ca18eb
judigot <judigot@github.com>
2022-04-28 11:05:33 +0800

    Commit

--- src/public/js/login.tsx
+++ src/public/js/login.tsx
@@ -2,13 +2,12 @@
  * DON'T MIX NODE.JS AND WEB JAVASCRIPT (WEBPACK CAN'T BUNDLE SEQUELIZE) *
  *************************************************************************/
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import { LoginForm } from "components/LoginForm";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("loginForm")!).render(
   <React.StrictMode>
     <LoginForm />
-  </React.StrictMode>,
-  document.getElementById("loginForm")
+  </React.StrictMode>
 );
