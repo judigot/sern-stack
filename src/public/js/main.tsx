20b74a089af3bd684ecdbc74824f1bedc8d03016
judigot <judigot@github.com>
2023-02-09 23:19:00 +0800

    Removed react usage

--- src/public/js/main.tsx
+++ src/public/js/main.tsx
@@ -4,13 +4,10 @@
 
 import Person from "Classes/Person";
 
-import { Component as ParentComponent } from "components/react";
 
 import "styles/main.scss";
-
-ParentComponent("root");
 
 var jude = new Person("Jude", "Francis");
 var personName = jude.getFullName();
 
-// alert(personName);
+alert(personName);
