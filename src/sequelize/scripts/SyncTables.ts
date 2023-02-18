e0c50da862933c81a938b456a36be5fbb46f8b65
judigot <judigot@github.com>
2023-02-18 21:57:19 +0800

    Move file

---
+++ src/sequelize/scripts/SyncTables.ts
@@ -0,0 +1,45 @@
+import { exit } from "process";
+
+import Model from "../models";
+
+Model.sequelize
+  .sync({
+    force: true,
+    // alter: true,
+    logging: false,
+  })
+  .then(function () {
+    console.log("Successfully updated table structure.");
+    exit();
+  })
+  .catch(function (error: any) {
+    console.log(error);
+  });
+
+// Model.sequelize
+//   .query("SET FOREIGN_KEY_CHECKS = 0;")
+//   .then(function () {
+//     // Re-build tables
+//     Model.sequelize
+//       .sync({
+//         force: true,
+//         // alter: true,
+//         logging: false,
+//       })
+//       .then(function () {
+//         Model.sequelize.query("SET FOREIGN_KEY_CHECKS = 1;").then(function () {
+//           console.log("Successfully updated table structure.");
+//           exit();
+//         });
+//       })
+//       .catch(function (error: any) {
+//         console.log(error);
+//       });
+//   })
+//   .catch((error: any) => {
+//     // Failure
+//     console.log(error);
+//   })
+//   .finally(() => {
+//     // Finally
+//   });
